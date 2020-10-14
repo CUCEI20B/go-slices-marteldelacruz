65ce2178c3899c422938355f6d2478772424f3f3
Martel <martel.mosqueda@alumnos.udg.mx>
2020-10-13 19:28:55 -0500

    Terminada la suma de elementos

--- main.go
+++ main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	// amout of slices
+	var total int
+	// sum result
+	var sum int = 0
+
+	// Read from user input
+	fmt.Scanln(&total)
+
+	// slice list
+	list := make([]int, total)
+
+	// read slices content
+	for i := 0; i < total; i++ {
+		fmt.Scanln(&list[i])
+	}
+
+	// adding element wise
+	for i := 0; i < total; i++ {
+		sum += list[i]
+	}
+
+	// print total
+	fmt.Print(sum)
 }
